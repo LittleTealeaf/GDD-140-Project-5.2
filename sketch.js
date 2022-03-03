0d70a9936842d6a2c7cd33c0ba874e0fe259daa9
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-03 15:35:11 -0500

    buttons now render something

--- sketch.js
+++ sketch.js
@@ -1,13 +1,45 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
+class Vector2 {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Button {
+  constructor(startPos, endPos, size, rend) {
+    this.startPos = startPos;
+    this.endPos = endPos;
+    this.size = size;
+    this.rend = rend;
+  }
+
+  render(showui) {
+    var x = this.startPos.x * (1 - showui) + this.endPos.x * showui;
+    var y = this.startPos.y * (1 - showui) + this.endPos.y * showui;
+    this.rend(showui,x,y,this.size.x,this.size.y);
+  }
+
+  contains(showui,mx,my) {
+    var x = this.startPos.x * (1 - showui) + this.endPos.x * showui;
+    var y = this.startPos.y * (1 - showui) + this.endPos.y * showui;
+    return mx > x && mx < x + this.size.x && my > y && my < y + this.size.y;
+  }
+}
+
 const fps = 60;
-const smooth_ui = 0.3;
-const max_ui_delay = 3 * fps;
+const smooth_ui = 0.2;
+const max_ui_delay = 1.5 * fps;
 const grid_length = 20;
+
+const button_height = 50;
 
 const RUNNING = 0;
 const ADDING = 1;
 const REMOVING = 2;
+
+var BUTTON_PLAY;
 
 var showui = 0;
 var ui_delay = 0;
@@ -35,12 +67,20 @@ function setup() {
       length: cols
     }, () => Math.random() < 0.5)
   );
+
+  BUTTON_PLAY = new Button(new Vector2(5,-40), new Vector2(5,5), new Vector2(100,50),function(showui,x,y,w,h) {
+    fill(255,255,255,255 * showui);
+    rect(x,y,w,h);
+    fill(255,0,0,255 * showui);
+    rect(x + 1, y + 1, w - 2, h - 2);
+  });
 }
 
 function draw() {
   background(0);
   updateShowUI();
   renderBoard();
+  renderUI();
   if(frameCount%(fps / tickrate) == 0) {
     tick();
   }
@@ -102,4 +142,12 @@ function tick() {
   }
   //Update board to new board
   board = newBoard;
+}
+
+function renderUI() {
+  //Draw the start/stop button
+  fill(255,255,255,255 * showui);
+  noStroke();
+  // rect(5,-40 + 50 * showui,100,button_height);
+  BUTTON_PLAY.render(showui);
 }
